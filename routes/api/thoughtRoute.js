5802e018bd96338be0bf40d7d9e9265815dba274
jproff <jessicakproffitt@gmail.com>
2024-01-16 21:33:46 -0500

    routes

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { getAllThoughts } = require('../../controllers/thoughtController.js');
 // TODO: build routes for getAllThoughts,
 // TODO: getThoughtById,
 // TODO: createThought,
@@ -6,4 +7,20 @@ const router = require('express').Router();
 // TODO: deleteThought,
 // TODO: addReaction,
 // TODO: deleteReaction
+
+//get all Thoughts and create user
+router.route('/').get(getAllThoughts).post(createThought);
+
+//get, update, delete Thought by id
+router
+	.route('/:id')
+	.get(getThoughtById)
+	.put(updateThought)
+	.delete(deleteThought);
+
+//add or remove add reaction
+router.route('/:thoughtId/reaction');
+    .post(addReaction)
+    .delete(deleteReaction);
+
 module.exports = router;
--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const {
+	getAllUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+	addFriend,
+	removeFriend,
+} = require('../../controllers/userController.js');
 // TODO: build routes for getAllUsers,
 // TODO: getUserById,
 // TODO: createUser,
@@ -6,4 +15,14 @@ const router = require('express').Router();
 // TODO: deleteUser,
 // TODO: addFriend,
 // TODO: removeFriend
+
+//get all users and create user
+router.route('/').get(getAllUsers).post(createUser);
+
+//get, update, delete user by id
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+//add or remove addFriend
+router.route();
+
 module.exports = router;
